28b04b3e0d3c61eb40845f915791c28753076faa
Kyrill Iakovenko <83785189+i4k0b@users.noreply.github.com>
2026-01-30 18:03:15 +0300

    Create solution for msu-main-2025-6.c

---
+++ msu-main-2025/msu-main-2025-6.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+enum
+{
+    ALPHABET_NUM = 26,
+    FIXED_LEN = 100
+};
+
+#define ALPHABET "abcdefghijklmnopqrstuvwxyz"
+
+char *generative(int seed)
+{
+    char *first_var = malloc(FIXED_LEN),
+         *second_var = malloc(FIXED_LEN);
+    int index = 15;
+
+    int pos1 = 0, pos2 = 0;
+
+    while (pos1 < FIXED_LEN)
+    {
+        index = (index + seed) % ALPHABET_NUM;
+        first_var[pos1] = ALPHABET[index];
+        if (first_var[pos1] == 'i')
+        {
+            first_var[pos1] = '\0';
+            break;
+        }
+        ++pos1;
+    }
+    index = 8;
+    while (pos2 < FIXED_LEN - 1)
+    {
+        index = (index + seed) % ALPHABET_NUM;
+        second_var[pos2] = ALPHABET[index];
+        if (second_var[pos2] == 'b')
+        {
+            second_var[pos2] = '\0';
+            break;
+        }
+        ++pos2;
+    }
+    strcat(strcat(first_var, " "), second_var); free(second_var);
+    return first_var;
+}
+
+int main(void)
+{
+    int n; scanf("%d", &n);
+    printf("%s\n", generative(n));
+    return 0;
+}
